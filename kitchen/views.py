6c589d41658ffcd5f77988582152907d8d5ad06e
toomuchtearz <toomuchtearz@gmail.com>
2025-10-28 15:15:43 +0200

    create url and view for main page

--- kitchen/views.py
+++ kitchen/views.py
@@ -1,3 +1,26 @@
+from django.contrib.auth import get_user_model
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+from kitchen.models import Dish, Ingredient, DishType
+
+
+def index(request: HttpRequest) -> HttpResponse:
+    num_dishes = Dish.objects.count()
+    num_ingredients = Ingredient.objects.count()
+    num_dish_types = DishType.objects.count()
+    num_cooks = get_user_model().objects.count()
+    num_visits = request.session.get("num_visits", 0) + 1
+    request.session["num_visits"] = num_visits
+
+    return render(
+        request=request,
+        template_name="kitchen/index.html",
+        context = {
+            "num_dishes": num_dishes,
+            "num_ingredients": num_ingredients,
+            "num_dish_types": num_dish_types,
+            "num_cooks": num_cooks,
+            "num_visits": num_visits
+        }
+    )
