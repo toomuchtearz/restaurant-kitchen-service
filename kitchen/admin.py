5e100f2e5ed166669e12f121708d0be7a9ed22fa
toomuchtearz <toomuchtearz@gmail.com>
2025-10-28 15:49:39 +0200

    registed models in admin panel

--- kitchen/admin.py
+++ kitchen/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from kitchen.models import Dish, DishType, Ingredient, Cook
+
+
+@admin.register(DishType)
+class DishTypeAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Dish)
+class DishAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Ingredient)
+class IngredientAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Cook)
+class IngredientAdmin(UserAdmin):
+    pass
