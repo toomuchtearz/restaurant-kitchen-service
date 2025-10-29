c04693c1b3e3def9e9292492e572d80dea8fcb65
toomuchtearz <toomuchtearz@gmail.com>
2025-10-29 15:20:16 +0200

    add auth urls

--- restaurant_kitchen_service/urls.py
+++ restaurant_kitchen_service/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", include("kitchen.urls", namespace="kitchen"))
+    path("", include("kitchen.urls", namespace="kitchen")),
+    path("accounts/", include("django.contrib.auth.urls",))
 ]
