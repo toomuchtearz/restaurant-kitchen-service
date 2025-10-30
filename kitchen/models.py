685dc83251d9c8675a6530b91ea37a8583d0a693
toomuchtearz <toomuchtearz@gmail.com>
2025-10-30 15:46:36 +0200

    fix PEP8 issues

--- kitchen/models.py
+++ kitchen/models.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+
 class DishType(models.Model):
     name = models.CharField(max_length=63)
 
